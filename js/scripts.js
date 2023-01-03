0848c0af8dc8168304ed6ce3956b0df7106376e7
Schlumen <lumen.haendler@web.de>
2023-01-03 20:03:05 +0100

    Add Pokemon array with Pokemon objects

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,7 @@
-alert("Hello world");
-let favoriteFood = "Chicken with rice and broccoli";
-document.write(favoriteFood);
+let pokemonList = [];
+pokemonList = [
+    {name: "Wartortle", height: 1, types: ["water"]},
+    {name: "Charizard", height: 1.7, types: ["fire", "flying"]},
+    {name: "Pikachu", height: 0.4, types: ["electric"]},
+    {name: "Nidoking", height: 1.4, types: ["ground", "poison"]}
+];
