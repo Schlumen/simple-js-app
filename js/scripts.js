1a0dd06f7c92fd80bc8d9af4b2c7d1dbabf0ae58
Schlumen <lumen.haendler@web.de>
2023-01-05 19:41:59 +0100

    Add loop to list all pokemons, add basic styling

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,14 @@ pokemonList = [
     {name: "Pikachu", height: 0.4, types: ["electric"]},
     {name: "Nidoking", height: 1.4, types: ["ground", "poison"]}
 ];
+
+// Print each pokemon on the website with their height
+for (let i = 0; i < pokemonList.length; i++){
+    document.write(`${pokemonList[i].name} (height: ${pokemonList[i].height})`);
+    // Label all huge pokemons
+    if (pokemonList[i].height > 1.5){
+        document.write(" - It's so huge!<br>");
+    } else {
+        document.write("<br>");
+    }
+}
