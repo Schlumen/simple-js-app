954ad09e646e2ee612541b8267db979a7b7bc999
Schlumen <lumen.haendler@web.de>
2023-01-08 10:32:03 +0100

    Wrap pokemonList array in an IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,29 @@
-let pokemonList = [];
-pokemonList = [
-    {name: "Wartortle", height: 1, types: ["water"]},
-    {name: "Charizard", height: 1.7, types: ["fire", "flying"]},
-    {name: "Pikachu", height: 0.4, types: ["electric"]},
-    {name: "Nidoking", height: 1.4, types: ["ground", "poison"]}
-];
+let pokemonRepository = (function() {
+    let pokemonList = [
+        {name: "Wartortle", height: 1, types: ["water"]},
+        {name: "Charizard", height: 1.7, types: ["fire", "flying"]},
+        {name: "Pikachu", height: 0.4, types: ["electric"]},
+        {name: "Nidoking", height: 1.4, types: ["ground", "poison"]}
+    ];
+
+    // Return functions
+    function getAll() {
+        return pokemonList;
+    }
+
+    function add(pokemon) {
+        pokemonList.push(pokemon);
+    }
+
+    // Return object with the same names for keys as values
+    return {
+        getAll: getAll,
+        add: add
+    };
+})();
 
 // Print each pokemon on the website with their height
-pokemonList.forEach(function(pokemon) {
+pokemonRepository.getAll().forEach(function(pokemon) {
     let output = `<p>${pokemon.name} (height: ${pokemon.height})`;
     // Label all huge pokemons
     if (pokemon.height > 1.5){
