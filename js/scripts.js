1aaf0ed14a8e384bda3b23f19134b6f8ad9ab385
Schlumen <lumen.haendler@web.de>
2023-01-10 19:36:43 +0100

    Add addListItem function inside the IIFE, clean up

--- js/scripts.js
+++ js/scripts.js
@@ -25,27 +25,7 @@ let pokemonRepository = (function() {
         });
     }
 
-    // Return object with the same names for keys as values
-    return {
-        getAll: getAll,
-        add: add,
-        getByName: getByName
-    };
-})();
-
-// Add a pokemon to the array
-pokemonRepository.add({name: "Glumanda", height: 0.6, types: ["fire"]});
-
-// Print each pokemon on the website
-printToDocument(pokemonRepository.getAll());
-
-// Search for pokemons and print them
-// document.write("<h2>Pokemons containing \"ar\" in their names</h2>");
-// printToDocument(pokemonRepository.getByName("AR"));
-
-// Function to print pokemons on the website with their height
-function printToDocument(pokemons) {
-    pokemons.forEach(function(pokemon) {
+    function addListItem(pokemon) {
         let uPokemonList = document.querySelector(".pokemon-list");
         let listItem = document.createElement("li");
         let button = document.createElement("button");
@@ -53,5 +33,21 @@ function printToDocument(pokemons) {
         button.classList.add("pokemon-button");
         listItem.appendChild(button);
         uPokemonList.appendChild(listItem);
-    });
-}
+    }
+
+    // Return object with the same names for keys as values
+    return {
+        getAll: getAll,
+        add: add,
+        getByName: getByName,
+        addListItem: addListItem
+    };
+})();
+
+// Add a pokemon to the array
+pokemonRepository.add({name: "Glumanda", height: 0.6, types: ["fire"]});
+
+// Print each pokemon on the website
+pokemonRepository.getAll().forEach(function(pokemon) {
+    pokemonRepository.addListItem(pokemon);
+});
