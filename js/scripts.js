36a6218896ccafa2c0f954e35139bc27e748a30f
Schlumen <lumen.haendler@web.de>
2023-01-11 22:10:38 +0100

    Edit animation, add flexbox

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 let pokemonRepository = (function() {
     let pokemonList = [];
-    let apiUrl = "https://pokeapi.co/api/v2/pokemon/?limit=150";
+    let apiUrl = "https://pokeapi.co/api/v2/pokemon/?limit=1300";
 
     // Return functions
     function getAll() {
